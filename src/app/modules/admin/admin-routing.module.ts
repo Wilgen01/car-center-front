9bfb8ae33dafc921ba334deff5e393831fbac943
Wilgen Garcia <wilgengarcia2001@gmail.com>
2023-10-02 15:50:18 -0500

    setting routes

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminModule } from './admin.module';
+import { AdminLayoutComponent } from '../layout/admin-layout/admin-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./../dashboard/dashboard.module').then(m => m.DashboardModule),
+        title: 'App - Dashboard'
+      },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
